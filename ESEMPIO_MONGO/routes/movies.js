21d56cb0523b077061fea3d0bb4b445f608e490a
fabioventu <venturini.fabio@iisgalvanimi.edu.it>
2020-11-27 11:43:45 +0000

    05.3.1

--- ESEMPIO_MONGO/routes/movies.js
+++ ESEMPIO_MONGO/routes/movies.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var router = express.Router();
 
@@ -20,5 +19,23 @@ router.get('/', function (req, res, next) {
     });
    
 });
+
+router.get('/movie_from_title/:title', function (req, res, next) {
+    console.log(req.params); //Leggo i parametri passati all'url
+    title = req.params.title;
+    const uri = "mongodb+srv://fventurini:<password>@cluster0.zx7mf.mongodb.net/Cluster0?retryWrites=true&w=majority"
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
+        // eseguo una find sulla collection
+        collection.find({ 'title': `${title}` }).toArray((err, result) => {
+            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            else res.send(result);
+            client.close(); //Quando ho terminato la find chiudo la sessione con il db
+        }); //Eseguo la query e passo una funzione di callback
+
+    });
+});
+
 
 module.exports = router;
